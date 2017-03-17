a1c9948db2fd08539d2ba5a6eb12608388e6713e
zhongjian <1104785781@qq.com>
2017-03-17 13:40:07 +0800

    company test

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -38,7 +38,7 @@
     <body>
         <div class="container">
             <div class="content">
-                <div class="title">Laravel 5  zhongjian</div>
+                <div class="title">Laravel 5  zhongjian company test</div>
             </div>
         </div>
     </body>
